b8c58445a13e6bd3ddd27051b5111bc983a0a919
AmeliaChady <ameliachady@gmail.com>
2020-02-06 16:21:37 -0500

    findID Declaration and Javadoc

--- src/main/java/edu/ithaca/dragon/bank/AccountLibrary.java
+++ src/main/java/edu/ithaca/dragon/bank/AccountLibrary.java
@@ -4,4 +4,13 @@ import java.util.List;
 
 public class AccountLibrary {
     private List<BankAccount> accounts;
+
+    /**
+     * @param accountID id of a bank account
+     * @return the bank account associated with the id
+     * @throws IllegalArgumentException if accountID DNE
+     */
+    public BankAccount findID(String accountID){
+
+    }
 }
