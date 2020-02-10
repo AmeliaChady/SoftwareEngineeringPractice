5ac3309321b2fca65d758f75edf0cac3ec313315
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2020-02-10 13:40:01 -0500

    acctlib work

--- src/main/java/edu/ithaca/dragon/bank/AccountLibrary.java
+++ src/main/java/edu/ithaca/dragon/bank/AccountLibrary.java
@@ -1,8 +1,44 @@
 package edu.ithaca.dragon.bank;
 
 
+import java.util.LinkedList;
 import java.util.Map;
+
+import static edu.ithaca.dragon.bank.Utilities.isAccountIDValid;
+import static edu.ithaca.dragon.bank.Utilities.isAmountValid;
 
 public class AccountLibrary {
     private Map<String, BankAccount> accounts;
+
+
+    public double getBalance(String accountID){
+        BankAccount account = accounts.get(accountID);
+        double balance = account.getBalance();
+        return balance;
+    }
+
+    public void createAccount(String accountID, double startingBalance, String accountType){
+        if (accountType != "savings" || accountType != "checking"){
+            throw new IllegalArgumentException("Account must be either 'checking' or 'savings'");
+        }
+        if(!isAccountIDValid(accountID)){
+            throw new IllegalArgumentException("Account ID "+accountID+" is Invalid!");
+        }
+        if(!isAmountValid(startingBalance)){
+            throw new IllegalArgumentException("Balance: " + startingBalance + " is invalid, cannot create account");
+        }
+        this.accountID = accountID;
+        this.balance = startingBalance;
+        this.history = new LinkedList<>();
+    }
+
+    public void closeAccount(String accountID, String accountType){
+        if (accountType != "savings" || accountType != "checking"){
+            throw new IllegalArgumentException("Account must be either 'checking' or 'savings'");
+        }
+        if(!isAccountIDValid(accountID)){
+            throw new IllegalArgumentException("Account ID "+accountID+" is Invalid!");
+        }
+
+    }
 }
