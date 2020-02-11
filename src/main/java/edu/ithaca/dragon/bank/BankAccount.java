b963f082e76ef256c00c70822b79ee8faf7740b5
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2020-02-11 13:55:42 -0500

    check credential test

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -15,6 +15,10 @@ public abstract class BankAccount {
         return false;
     }
 
+    public String getPassword(){
+        return password;
+    }
+
     /**
      * Withdraws money from the account
      * @param amount amount to withdraw
