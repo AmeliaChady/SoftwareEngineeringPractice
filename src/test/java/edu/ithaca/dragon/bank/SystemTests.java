826f9716096d5c6aaae13942c351e3f03bcea4cf
AmeliaChady <ameliachady@gmail.com>
2020-02-10 19:27:48 -0500

    test setup

--- src/test/java/edu/ithaca/dragon/bank/SystemTests.java
+++ src/test/java/edu/ithaca/dragon/bank/SystemTests.java
@@ -12,7 +12,38 @@ public class SystemTests {
 
     //---------------Amelia---------------//
     @Test
-    public void suspiciousDayTest(){
+    public void suspiciousDaysTest(){
+        CentralBank cb = new CentralBank();
+        // Pretend Beforehand
+        cb.createAccount("act0", 171);
+        cb.createAccount("act1", 172);
+        cb.createAccount("act2", 220);
+        cb.createAccount("act3", 345);
+
+        BasicAPI atm = cb;
+        AdvancedAPI teller = cb;
+        AdminAPI admin = cb;
+        DaemonAPI daemon = cb;
+
+        // Day 1
+
+
+
+
+
+
+        // Day 1 End Asserts
+
+
+
+        // Day 2
+
+
+
+
+        // Day 2 End Asserts
+
+
 
     }
 
