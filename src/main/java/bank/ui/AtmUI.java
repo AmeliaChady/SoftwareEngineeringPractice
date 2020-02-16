7f8a6f8cb17313d7277962148293bd35e1f0b467
AmeliaChady <ameliachady@gmail.com>
2020-02-16 16:22:30 -0500

    state WAITING implemented (NEEDS TESTING)

--- src/main/java/bank/ui/AtmUI.java
+++ src/main/java/bank/ui/AtmUI.java
@@ -1,11 +1,57 @@
 package bank.ui;
 
+import edu.ithaca.dragon.bank.AccountFrozenException;
 import edu.ithaca.dragon.bank.BasicAPI;
 import edu.ithaca.dragon.bank.CentralBank;
 
+import java.util.Scanner;
+
+
 public class AtmUI {
+    private enum AtmState{
+        WAITING,
+        LOGGEDIN,
+        DEPOSIT_INPUT,
+        WITHDRAW_INPUT,
+        TRANSACTION_INPUT
+    }
 
     public static void main(String[] args) {
         BasicAPI api = CentralBank.testBank();
+        AtmState state = AtmState.WAITING;
+        Scanner user = new Scanner(System.in);
+        String currentAccount = null;
+
+        while(true){
+            switch (state){
+                case WAITING:
+                    System.out.print("Account ID: ");
+                    String acctID = user.nextLine();
+                    System.out.print("Password: ");
+                    String pass = user.nextLine();
+
+                    try {
+                        if (api.confirmCredentials(acctID.trim(), pass.trim())) {
+                            currentAccount = acctID;
+                            state = AtmState.LOGGEDIN;
+                        }
+                    }
+                    catch (AccountFrozenException e){
+                        System.out.println("Account "+acctID+" is FROZEN. " +
+                                "\n Contact Customer Services at " +
+                                "\n ~~ 1-888-555-1212 ~~");
+                    }
+                    break;
+
+                case LOGGEDIN:
+                    System.out.println("HERE");
+
+                case DEPOSIT_INPUT:
+
+                case WITHDRAW_INPUT:
+
+                case TRANSACTION_INPUT:
+            }
+        }
     }
 }
